c18331c7dad542ca4d8f8349806b922907a1c354
samnewga <newgate90s@gmail.com>
2020-02-28 14:13:25 -0700

    accordion wireframe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './TabComponents/Accordion';
 
+const placeholder =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris' +
+  'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in' +
+  'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla' +
+  'pariatur. Excepteur sint occaecat cupidatat non proident, sunt in' +
+  'culpa qui officia deserunt mollit anim id est laborum.';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion title="Patient Information">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
+      <Accordion title="Social History">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
+      <Accordion title="Anthropometrics">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
+      <Accordion title="Medical Condition">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
+      <Accordion title="Medication">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
+      <Accordion title="Past Medical Care">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
+      <Accordion title="Substance Use">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
+      <Accordion title="Physical History">
+        <span className="accordion-text">{placeholder}</span>
+      </Accordion>
     </div>
   );
 }
